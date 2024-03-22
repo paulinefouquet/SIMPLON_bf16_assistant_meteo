2bf4de12e79e0ae436da899229d5c9555972db9a
Pauline F <pauline.fouquet.dev@gmail.com>
2024-03-22 15:23:06 +0100

    fichier de test

--- get_meteo.py
+++ get_meteo.py
@@ -2,10 +2,9 @@ from meteofrance_api import MeteoFranceClient
 
 meteo = MeteoFranceClient()
 
-
 def city_meteo_forecast(lat, long):
     try:
         city_forecast = meteo.get_forecast(lat, long)
         return city_forecast.forecast
-    except:
-        print("erreur lors de l'appel à l'API météoFrance")           
+    except Exception as e:
+        print(f"erreur lors de l'appel à l'API météoFrance: {e}")           
---
+++ test_.py
@@ -0,0 +1,16 @@
+import pytest
+from get_meteo import city_meteo_forecast
+
+def test_city_meteo_forecast_valid_input():
+
+    lat = 48.8566  # Latitude de Paris
+    long = 2.3522   # Longitude de Paris
+
+    result = city_meteo_forecast(lat, long)
+
+    # Vérification si le résultat n'est pas vide
+    assert result != None
+    assert isinstance(result, list)
+    assert len(result) == 76
+    assert 'dt' in result[0]
+    assert (result[0]['dt'] % 100) == 0
