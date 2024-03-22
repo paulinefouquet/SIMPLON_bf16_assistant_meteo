0ea9b3d59d16b89dfb2ae5754a63ecb8aaf2608a
Pauline F <pauline.fouquet.dev@gmail.com>
2024-03-22 12:06:25 +0100

    rajout fonction delete

--- db.py
+++ db.py
@@ -3,16 +3,22 @@ import psycopg2
 from config import DB_NAME, USER, PASSWORD, HOST, PORT
 
 def connect_to_db():
-    conn = psycopg2.connect(
-        dbname=DB_NAME,
-        user=USER,
-        password=PASSWORD,
-        host=HOST,
-        port=PORT
-    )
-    print("Successfully connected to PostgreSQL!")
-    cur = conn.cursor()
-    return conn, cur
+    try:
+        conn = psycopg2.connect(
+            dbname=DB_NAME,
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
+            port=PORT
+        )
+        print("Successfully connected to PostgreSQL!")
+        cur = conn.cursor()
+        return conn, cur
+
+    except Exception as e:
+        conn.rollback()  # Rollback the transaction if an error occurs
+        print(f"Error connecting data into PostgreSQL: {e}")
+
 
 def fetch_perimeter_dep(cur, dep_name):
     cur.execute("""
@@ -21,6 +27,7 @@ def fetch_perimeter_dep(cur, dep_name):
         WHERE department_name = %s;
     """, (dep_name,))
     return cur.fetchall()
+
 
 def create_table(cur):
     cur.execute("""
@@ -46,29 +53,51 @@ def create_table(cur):
 
 def insert_data(conn, cur, data, city_data, dt_ref):
     latitude, longitude, label = city_data
+    
+    try:
+        for item in data:
 
-    for item in data:
+            dt_forecast = item['dt']
+            temperature = item['T']['value']
+            humidity = item['humidity']
+            sea_level = item['sea_level']
+            wind_speed = item['wind']['speed']
+            wind_gust = item['wind']['gust']
+            wind_direction = item['wind']['direction']
+            weather_icon = item['weather']['icon']
+            weather_desc = item['weather']['desc']
 
-        dt_forecast = item['dt']
-        temperature = item['T']['value']
-        humidity = item['humidity']
-        sea_level = item['sea_level']
-        wind_speed = item['wind']['speed']
-        wind_gust = item['wind']['gust']
-        wind_direction = item['wind']['direction']
-        weather_icon = item['weather']['icon']
-        weather_desc = item['weather']['desc']
+            try:
+                cur.execute("""
+                    INSERT INTO weather2 (dt_ref, dt_forecast, city_label, latitude, longitude, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+                """, (dt_ref, dt_forecast, label, latitude, longitude, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc))
 
-        try:
-            cur.execute("""
-                INSERT INTO weather2 (dt_ref, dt_forecast, city_label, latitude, longitude, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-            """, (dt_ref, dt_forecast, label, latitude, longitude, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc))
+                print("insertion des données en cours")
 
-            print("insertion des données en cours")
+                conn.commit()
 
-            conn.commit()
-            
-        except psycopg2.IntegrityError:
-            conn.rollback()
-            print("ce jeu de données a déja été chargé.")
+            except psycopg2.IntegrityError:
+                conn.rollback()
+                print("ce jeu de données a déja été chargé.")
+
+    except Exception as e:
+        conn.rollback()  # Rollback the transaction if an error occurs
+        print(f"Error inserting data into PostgreSQL: {e}")
+
+
+def delete_old_data_30h(conn, cur):
+    try:
+        cur.execute("""
+            DELETE FROM weather2
+            WHERE EXTRACT(EPOCH FROM NOW()) - dt_ref > 108000;
+        """)
+        conn.commit()
+
+    except Exception as e:
+        conn.rollback()
+        print(f"Error deleting data older than 10 hours: {e}")
+    finally:
+        cur.close()
+
+
--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-import time
 
-from db import connect_to_db, fetch_perimeter_dep, create_table, insert_data
+from db import connect_to_db, fetch_perimeter_dep, create_table, insert_data, delete_old_data_30h
 from meteo import city_meteo_forecast
 
 DEPARTMENT_PERIMETER = 'hérault'
 
-try:
-    conn, cur = connect_to_db()
-    cities_data = fetch_perimeter_dep(cur, DEPARTMENT_PERIMETER)
+conn, cur = connect_to_db()
 
-    try:
-        create_table(cur)
+cities_data = fetch_perimeter_dep(cur, DEPARTMENT_PERIMETER)
 
-        for city_data in cities_data:
-            latitude, longitude, label = city_data
-            data = city_meteo_forecast(latitude, longitude)
+create_table(cur)
 
-            # créer un dt_ref unique pour chaque jeu donnée téléchargé
-            dt_ref = min(data, key=lambda x: x['dt'])['dt']
+for city_data in cities_data:
 
-            insert_data(conn, cur, data, city_data, dt_ref)
-        print(f"Data transfer to PostgreSQL succeeded! Dt_load :{dt_ref}")
+    latitude, longitude, label = city_data
+    data_meteo = city_meteo_forecast(latitude, longitude)
 
-    except Exception as e:
-        conn.rollback()  # Rollback the transaction if an error occurs
-        print(f"Error inserting data into PostgreSQL: {e}")
-finally:
-    # Close cursor and connection
-    cur.close()
-    conn.close()
+    # créer un dt_ref unique pour chaque jeu donnée téléchargé
+    dt_ref = min(data_meteo, key=lambda x: x['dt'])['dt']
+
+    insert_data(conn, cur, data_meteo, city_data, dt_ref)
+
+print(f"Data transfer to PostgreSQL succeeded! Dt_load :{dt_ref}")
+
+delete_old_data_30h(conn, cur)
+print(f"Old data suppressed")
+
+# Close cursor and connection
+cur.close()
+conn.close()
