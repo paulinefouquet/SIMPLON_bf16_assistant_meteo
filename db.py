9aad20078022bffe241c487489ced7d9f80e345a
Pauline F <pauline.fouquet.dev@gmail.com>
2024-03-22 10:50:40 +0100

    code fonctionnel

--- db.py
+++ db.py
@@ -25,9 +25,12 @@ def fetch_perimeter_dep(cur, dep_name):
 def create_table(cur):
     # Create table if not exists
     cur.execute("""
-        CREATE TABLE IF NOT EXISTS weather (
+        CREATE TABLE IF NOT EXISTS weather2 (
             dt_ref INT,
             dt_forecast INT,
+            city_label VARCHAR(100),
+            latitude FLOAT,
+            longitude FLOAT,
             temperature FLOAT,
             humidity INT,
             sea_level FLOAT,
@@ -36,32 +39,12 @@ def create_table(cur):
             wind_direction INT,
             weather_icon VARCHAR(10),
             weather_desc VARCHAR(100),
-            latitude FLOAT,
-            longitude FLOAT,
-            label VARCHAR(100)
+            CONSTRAINT pk_weather2 PRIMARY KEY (dt_ref, dt_forecast, latitude, longitude)
         );
     """)
 
-# def check_data(cur, dt_ref, longitude, latitude, label):
-#     # Vérifier si dt_ref existe déjà pour la combinaison de longitude, latitude et label
-#     cur.execute("""
-#         SELECT COUNT(*) FROM weather
-#         WHERE dt_ref = %s AND longitude = %s AND latitude = %s AND label = %s;
-#     """, (dt_ref, longitude, latitude, label))
-
-#     # Récupérer le compte
-#     count = cur.fetchone()[0]
-#     return count
-
 def insert_data(conn, cur, dt_ref, item, city_data):
-    longitude, latitude, label = city_data
-
-    # Vérifier si les données existent déjà
-    # count = check_data(cur, dt_ref, longitude, latitude, label)
-
-    # if count > 0:
-    #     print(f"Les données pour dt_ref {dt_ref} existent déjà pour {label} ({latitude}, {longitude}). L'insertion est ignorée.")
-    # else:
+    latitude, longitude, label = city_data
 
     dt_forecast = item['dt']
     temperature = item['T']['value']
@@ -72,11 +55,15 @@ def insert_data(conn, cur, dt_ref, item, city_data):
     wind_direction = item['wind']['direction']
     weather_icon = item['weather']['icon']
     weather_desc = item['weather']['desc']
-        
-    cur.execute("""
-        INSERT INTO weather (dt_ref, dt_forecast, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc, longitude, latitude, label)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-    """, (dt_ref, dt_forecast, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc, longitude, latitude, label))
 
-    # Commit the transaction
-    conn.commit()
+    print("insertion des données en cours")
+    try:
+        cur.execute("""
+            INSERT INTO weather2 (dt_ref, dt_forecast, city_label, latitude, longitude, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+        """, (dt_ref, dt_forecast, label, latitude, longitude, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc))
+        conn.commit()
+        print("Données insérées avec succès.")
+    except psycopg2.IntegrityError:
+        conn.rollback()
+        print("Erreur : Cette combinaison de valeurs pour dt_ref, dt_forecast, latitude et longitude existe déjà.")
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import time
+import psycopg2
 
 from db import connect_to_db, fetch_perimeter_dep, create_table
 from meteo import city_meteo_forecast
 
-DEPARTMENT_PERIMETER = 'hérault'
-timestamp_actuel = int(time.time())
+DEPARTMENT_PERIMETER = 'gard'
 
 try:
     conn, cur = connect_to_db()
@@ -17,44 +17,36 @@ try:
             latitude, longitude, label = city_data
             data = city_meteo_forecast(latitude, longitude)
 
-            # créer une clé de chargement dt_ref qui correspond au min dt des données chargeé
-
+            # créer un dt_ref unique pour chaque jeu donnée téléchargé
             dt_ref = min(data, key=lambda x: x['dt'])['dt']
 
-            # Vérifier si les données existent déjà
+            # Insert data into the table
+            for item in data:
 
-            cur.execute("""
-                SELECT COUNT(*) FROM weather
-                WHERE dt_ref = %s AND longitude = %s AND latitude = %s AND label = %s;
-            """, (dt_ref, latitude, longitude, label))
+                dt_forecast = item['dt']
+                temperature = item['T']['value']
+                humidity = item['humidity']
+                sea_level = item['sea_level']
+                wind_speed = item['wind']['speed']
+                wind_gust = item['wind']['gust']
+                wind_direction = item['wind']['direction']
+                weather_icon = item['weather']['icon']
+                weather_desc = item['weather']['desc']
 
-            count = cur.fetchone()[0]
-
-            if count > 0:
-                print(f"Les données pour dt_ref {dt_ref} existent déjà pour {label} ({latitude}, {longitude}). L'insertion est ignorée.")
-            
-            else:
-                # Insert data into the table
-                for item in data:
-
-                    dt_forecast = item['dt']
-                    temperature = item['T']['value']
-                    humidity = item['humidity']
-                    sea_level = item['sea_level']
-                    wind_speed = item['wind']['speed']
-                    wind_gust = item['wind']['gust']
-                    wind_direction = item['wind']['direction']
-                    weather_icon = item['weather']['icon']
-                    weather_desc = item['weather']['desc']
-                        
+                try:
                     cur.execute("""
-                        INSERT INTO weather (dt_ref, dt_forecast, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc, longitude, latitude, label)
+                        INSERT INTO weather2 (dt_ref, dt_forecast, city_label, latitude, longitude, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc)
                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-                    """, (dt_ref, dt_forecast, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc, longitude, latitude, label))
+                    """, (dt_ref, dt_forecast, label, latitude, longitude, temperature, humidity, sea_level, wind_speed, wind_gust, wind_direction, weather_icon, weather_desc))
 
-                    # Commit the transaction
+                    print("insertion des données en cours")
+
                     conn.commit()
+                except psycopg2.IntegrityError:
+                    conn.rollback()
+                    print("ce jeu de données a déja été chargé.")
 
+        timestamp_actuel = int(time.time())
         print(f"Data transfer to PostgreSQL succeeded! Dt_load :{timestamp_actuel}")
 
     except Exception as e:
